365d4baf1e96f07f4021cb0af07dfbb4f0b4a7b8
ichadhr <ichadhr@gmail.com>
2024-11-22 13:58:53 +0700

    add node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapterNode from '@sveltejs/adapter-node';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -11,7 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapterNode(),
 		alias: {
 			"@/*": "./src/*",
 		},
